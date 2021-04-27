b02809de78b4d06fe12bedc1d6d8718c533418be
yogesh <yvrushabendrappa@sonicwall.com>
2021-04-27 20:14:39 +0530

    intial scripts to implement worker queues in rabbitMQ

---
+++ worker.py
@@ -0,0 +1,30 @@
+import pika 
+import time
+
+# creates a connection instance to rabbitMQ
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+# creates a channel to communicate to rabbitMQ server
+channel = connection.channel()
+
+# checks if the recipient queue already exists, if not creates it
+# makes the queue as durable, even if rabbitMQ crashes
+channel.queue_declare(queue='task_queue', durable=True)
+
+print("[*] Waiting for messages. To exit press CTRL+c")
+
+def callback(ch, method, properties, body):
+    print("[x] Received message {}".format(body))
+    time.sleep(body.count(b'.'))
+    print("[x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+# This uses the basic.qos protocol method to tell rabbitMQ to 
+# not give more than one message to a worker at a time
+channel.basic_qos(prefetch_count=1)
+
+# receive the message from queue and specify the callback function
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
+
+channel.start_consuming()
+
